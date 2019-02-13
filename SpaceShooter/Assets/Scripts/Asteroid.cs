af852c8599b2d54697d3c52f94df3720d08412c0
Redd <redd.caruana@mcast.edu.mt>
2019-02-13 15:23:53 +0100

    Basic Physics
    Moving Asteroid with physics.
    Moving Player Ship with physics.
    Added collision between player and asteroid.

--- SpaceShooter/Assets/Scripts/Asteroid.cs
+++ SpaceShooter/Assets/Scripts/Asteroid.cs
@@ -2,6 +2,8 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+// This code enforces the use of a Rigidbody2D component.
+[RequireComponent(typeof(Rigidbody2D))]
 public class Asteroid : MonoBehaviour
 {
 	// The Asteroid's movement speed.
@@ -10,9 +12,26 @@ public class Asteroid : MonoBehaviour
 	// The Asteroid's rotation speed.
 	public float torque = 180f;
 
-	void Update()
+	// The Asteroid's Rigidbody2D component
+	private Rigidbody2D _asteroidRB;
+
+	// Any components we'll need to reference later on,
+	// should be acquired when the script wakes up.
+	private void Awake()
 	{
-		transform.Translate(Vector3.up * speed * Time.deltaTime);
-		transform.Rotate(Vector3.back * torque * Time.deltaTime);
+		// We're looking for the Rigidbody2D component
+		// on this object, and taking reference.
+		_asteroidRB = GetComponent<Rigidbody2D>();
+	}
+
+	// When the game/scene starts (after Awake), this
+	// is what it will do.
+	private void Start()
+	{
+		_asteroidRB.AddForce(Random.insideUnitCircle * speed,
+			ForceMode2D.Impulse);
+		
+		_asteroidRB.AddTorque((Random.value - 0.5f) * torque,
+			ForceMode2D.Impulse);
 	}
 }
--- SpaceShooter/Assets/Scripts/PlayerControls.cs
+++ SpaceShooter/Assets/Scripts/PlayerControls.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+[RequireComponent(typeof(Rigidbody2D))]
 // A class for a Unity GameObject extends from MonoBehaviour.
 // This will enable code to control an object in the scene.
 public class PlayerControls : MonoBehaviour
@@ -12,19 +13,35 @@ public class PlayerControls : MonoBehaviour
 	// The player's rotation speed.
 	public float torque = 360f;
 
+	// A reference to the Rigidbody2D component.
+	private Rigidbody2D _playerRB;
+
+	// We'll keep the direction as a separate variable.
+	private Vector2 _direction;
+
+	private void Awake()
+	{
+		_playerRB = GetComponent<Rigidbody2D>();
+	}
+
 	// This function will execute for every frame.
 	void Update()
 	{
 		// Will read the input on the Up/Down or W/S keys.
-		float vert = Input.GetAxis("Vertical");
-
-		// Will move the player vertically.
-		transform.Translate(Vector3.up * vert * speed * Time.deltaTime);
+		_direction = transform.TransformDirection(Vector2.up)
+			*  Input.GetAxis("Vertical");
 
 		// Will read the input on the Left/Right or A/D keys.
 		float horz = Input.GetAxis("Horizontal");
 
 		// Will rotate the player.
 		transform.Rotate(Vector3.back * horz * torque * Time.deltaTime);
+	}
+
+	// Use this every time we move things via Rigidbody
+	private void FixedUpdate()
+	{
+		_playerRB.MovePosition(_playerRB.position
+			+ _direction * speed * Time.deltaTime);
 	}
 }
