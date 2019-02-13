e316f58222640fc524084057a7f72d635b92a9b8
Redd <redd.caruana@mcast.edu.mt>
2019-02-13 14:20:11 +0100

    Spaceship and Asteroid
    Lesson 1 - Moving a spaceship and asteroid.

---
+++ SpaceShooter/Assets/Scripts/Asteroid.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Asteroid : MonoBehaviour
+{
+	// The Asteroid's movement speed.
+	public float speed = 1f;
+
+	// The Asteroid's rotation speed.
+	public float torque = 180f;
+
+	void Update()
+	{
+		transform.Translate(Vector3.up * speed * Time.deltaTime);
+		transform.Rotate(Vector3.back * torque * Time.deltaTime);
+	}
+}
---
+++ SpaceShooter/Assets/Scripts/PlayerControls.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// A class for a Unity GameObject extends from MonoBehaviour.
+// This will enable code to control an object in the scene.
+public class PlayerControls : MonoBehaviour
+{
+	// The player's movement speed.
+	public float speed = 10f;
+
+	// The player's rotation speed.
+	public float torque = 360f;
+
+	// This function will execute for every frame.
+	void Update()
+	{
+		// Will read the input on the Up/Down or W/S keys.
+		float vert = Input.GetAxis("Vertical");
+
+		// Will move the player vertically.
+		transform.Translate(Vector3.up * vert * speed * Time.deltaTime);
+
+		// Will read the input on the Left/Right or A/D keys.
+		float horz = Input.GetAxis("Horizontal");
+
+		// Will rotate the player.
+		transform.Rotate(Vector3.back * horz * torque * Time.deltaTime);
+	}
+}
