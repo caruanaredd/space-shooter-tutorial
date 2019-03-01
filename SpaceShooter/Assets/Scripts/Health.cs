d56cfd9f6bffffec6331571ab8ccdfe9bcf70a5c
Redd <redd.caruana@mcast.edu.mt>
2019-03-01 14:09:49 +0100

    Shield Mechanics
    Temporary invincibility when the player hits an asteroid.
    
    GUI Setup and Slider to show player health.

--- SpaceShooter/Assets/Scripts/Health.cs
+++ SpaceShooter/Assets/Scripts/Health.cs
@@ -8,6 +8,21 @@ public class Health : MonoBehaviour
     [Range(0f, 100f)]
     public float health = 100f;
 
+    // We'll use a reference to the shield slider to show health.
+    public ShieldSlider shieldSlider;
+
+    // A reference to the shield gameobject on the scene.
+    public GameObject shield;
+
+    // How long will the shield be active for in seconds?
+    public float shieldDuration = 1f;
+
+    private void Start()
+    {
+        shieldSlider.SetValue(health);
+        StartCoroutine(ActivateShield());
+    }
+
     private void OnCollisionEnter2D(Collision2D other)
     {
         // We'll only damage the player if we hit an asteroid.
@@ -18,9 +33,31 @@ public class Health : MonoBehaviour
 
             // <Asteroid> is used to enforce the type of script we need.
             Asteroid asteroid = other.gameObject.GetComponent<Asteroid>();
-            health -= asteroid.damage;
-
-            asteroid.Break();
+            
+            if (!shield.activeSelf)
+            {
+                Damage(asteroid.damage);
+                asteroid.Break();
+                StartCoroutine(ActivateShield());
+            }
         }
+    }
+
+    public void Damage(float damage)
+    {
+        health -= damage;
+        shieldSlider.SetValue(health);
+    }
+
+    // IEnumerators are timers that occupy a different thread to the game.
+    private IEnumerator ActivateShield()
+    {
+        shield.SetActive(true);
+
+        // this function will wait for 1 second before doing the rest
+        // of its commands.
+        yield return new WaitForSeconds(shieldDuration);
+
+        shield.SetActive(false);
     }
 }
---
+++ SpaceShooter/Assets/Scripts/ShieldSlider.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+[RequireComponent(typeof(Slider))]
+public class ShieldSlider : MonoBehaviour
+{
+    // A reference to the slider component on this same object.
+    private Slider _slider;
+
+    // A reference to the slider's fill image.
+    public Image sliderFill;
+
+    // A gradient showing what colors to use for the health slider.
+    public Gradient healthColor;
+
+    private void Awake()
+    {
+        _slider = GetComponent<Slider>();
+    }
+
+    // Will be used to set the slider value.
+    public void SetValue(float value)
+    {
+        _slider.value = value;
+        sliderFill.color = healthColor.Evaluate(_slider.value / _slider.maxValue);
+    }
+}
